a807645c9485602fc4eb9084e94fd6acfdd312ac
Kobbe <jakub.korban@microsoft.wsei.edu.pl>
2023-10-17 15:43:20 +0200

    side

---
+++ lab3a/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using lab3a.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab3a.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact model)
+        {
+            if(ModelState.IsValid)
+            {
+                
+                // dodanie modelu do aplikacji
+                return RedirectToAction("Index");
+            }
+            return View(); // ponownie wyswietl formularz z bledami
+        }
+    }
+}
---
+++ lab3a/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab3a.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Musisz podac imie!")]
+        [StringLength(maximumLength: 50, ErrorMessage = "Zbyt dlugie imie, ma 50 znakow")]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string? Phone { get; set; }          // pole opcjonalne
+        public DateTime? Birth { get; set; }        // pole opcjonalne
+    }
+}
