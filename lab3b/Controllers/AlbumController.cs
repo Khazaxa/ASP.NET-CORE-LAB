89d3eaeb80b21b00a6c248512b2d1baca0791dd8
Kobbe <korbanj1108@gmail.com>
2023-10-20 16:32:23 +0200

    Create AlbumController

--- lab3b/Controllers/AlbumController.cs
+++ lab3b/Controllers/AlbumController.cs
@@ -1,12 +1,92 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using lab3b.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace lab3b.Controllers
 {
     public class AlbumController : Controller
     {
+        private static Dictionary<int, Album> _albums = new Dictionary<int, Album>();
+
         public IActionResult Index()
         {
+            return View(_albums);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = _albums.Keys.Count != 0 ? _albums.Keys.Max() : 0;
+                album.Id = id + 1;
+                _albums.Add(album.Id, album);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(album);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (_albums.ContainsKey(id))
+            {
+                return View(_albums[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _albums[album.Id] = album;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(album);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (_albums.ContainsKey(id))
+            {
+                return View(_albums[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (_albums.ContainsKey(id))
+            {
+                _albums.Remove(id);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
