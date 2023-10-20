943b13f0c8db13463ea61bf924e9aa9e2bf0cc7c
Kobbe <korbanj1108@gmail.com>
2023-10-20 16:31:07 +0200

    Add Album model

---
+++ lab3b/Models/Album.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab3b.Models
+{
+    public class Album
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwę albumu.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwę zespołu.")]
+        public string Band { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać spis piosenek.")]
+        public string Tracklist { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać notowanie.")]
+        public string ChartPosition { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać datę wydania.")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać czas trwania.")]
+        public string Duration { get; set; }
+
+    }
+}
