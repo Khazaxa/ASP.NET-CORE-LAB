2e09cf835fcc6d59a404991dff44ec73b38b208c
Kobbe <korbanj1108@gmail.com>
2023-11-13 22:09:00 +0100

    add album to data

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -11,6 +11,7 @@ namespace Data
     public class AppDbContext : DbContext
     {
         public DbSet<ContactEntity> Contacts { get; set; }
+        public DbSet<AlbumEntity> Albums { get; set; }
         private string DbPath { get; set; }
         public AppDbContext()
         {
@@ -27,6 +28,11 @@ namespace Data
                 new ContactEntity() { Id = 1, Name = "Adam", Email = "adam@wsei.edu.pl", Phone = "127813268163", Birth = new DateTime(2000, 10, 10), Priority = 1 },
                 new ContactEntity() { Id = 2, Name = "Ewa", Email = "ewa@wsei.edu.pl", Phone = "293443823478", Birth = new DateTime(1999, 8, 10), Priority = 2}
             );
+
+            modelBuilder.Entity<AlbumEntity>().HasData(
+                new AlbumEntity() { Id = 1, Nazwa = "Album 1", Zespol = "Zespół 1", SpisPiosenek = "Piosenka 1, Piosenka 2", Notowanie = 1, DataWydania = new DateTime(2023, 1, 1), CzasTrwania = 3600 },
+                new AlbumEntity() { Id = 2, Nazwa = "Album 2", Zespol = "Zespół 2", SpisPiosenek = "Piosenka 3, Piosenka 4", Notowanie = 2, DataWydania = new DateTime(2023, 2, 1), CzasTrwania = 3000 }
+    );
         }
     }
 }
