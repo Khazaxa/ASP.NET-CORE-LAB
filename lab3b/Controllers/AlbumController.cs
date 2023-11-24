99a9c35b40f91a946485f43ea1d54e5a52023f31
Kobbe <korbanj1108@gmail.com>
2023-11-24 20:37:42 +0100

    refactored album todo: songlist

---
+++ lab3b/Controllers/AlbumController.cs
@@ -0,0 +1,86 @@
+﻿using lab3b.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab3b.Controllers;
+
+public class AlbumController : Controller
+{
+    private static readonly Dictionary<int, Album> _albums = new Dictionary<int, Album>();
+    
+    public IActionResult Index()
+    {
+        return View(_albums);
+    }
+    
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create([Bind("Name, Band, ChartRanking, ReleaseDate, Duration")] Album album, string[] songs)
+    {
+        if (ModelState.IsValid)
+        {
+            album.SongList = new Dictionary<int, string>();
+            
+            int songId = 1;
+            foreach (var song in songs)
+            {
+                if (!string.IsNullOrWhiteSpace(song))
+                {
+                    album.SongList.Add(songId, song);
+                    songId++;
+                }
+            }
+
+            album.Id = _albums.Count + 1;
+            _albums.Add(album.Id, album);
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(album);
+    }
+
+    public IActionResult Edit(int id)
+    {
+        if (!_albums.ContainsKey(id))
+        {
+            return NotFound();
+        }
+
+        return View(_albums[id]);
+    }
+    
+    public IActionResult Details(int id)
+    {
+        if (_albums.ContainsKey(id))
+        {
+            var album = _albums[id];
+            return View(album);
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+    
+    public IActionResult Delete(int id)
+    {
+        if (!_albums.ContainsKey(id))
+        {
+            return NotFound();
+        }
+
+        return View(_albums[id]);
+    }
+    
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        _albums.Remove(id);
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ lab3b/Models/Album.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace lab3b.Models;
+
+public class Album
+{
+    [HiddenInput]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać nazwę albumu!")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać nazwę zespołu!")]
+    public string Band { get; set; }
+    
+    public Dictionary<int, string> SongList { get; set; } = new Dictionary<int, string>();
+
+    [Required(ErrorMessage = "Proszę podać datę wydania albumu!")]
+    public int ChartRanking { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać numer w notowaniu!")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać czas trwania albumu!")]
+    [RegularExpression(@"^\d{1,2}:\d{2}$", ErrorMessage = "Czas trwania musi być w formacie HH:MM.")]
+    public string Duration { get; set; }
+ 
+}
